540a038d5639aca840639e4fb6ecf439dc931815
Hafiz Haziq <hafizhaziq.dev@gmail.com>
2023-01-29 03:16:15 +0800

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -8,3 +8,16 @@ function isEmpty(val) {
   if (typeof val === "object") return !Object.keys(val).length;
   return !val;
 }
+
+/**
+ * Replace value if string value is empty
+ * @param {string} val
+ * @param {string} replaceWith
+ * @returns {string}
+ */
+function setDefaultStr(val, replaceWith) {
+  if (typeof val === "string" || val === null || val === undefined) {
+    return !val ? replaceWith : val;
+  }
+  return val;
+}
